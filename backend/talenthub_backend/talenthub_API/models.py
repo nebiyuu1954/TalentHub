026539bfd74bbcf129029c62034245d2dfb344ed
Nebiyu Main <nebiyuesaiyas1954@gmail.com>
2025-08-26 10:14:21 +0300

    refactored and updated the model user, job and appliations

--- backend/talenthub_backend/talenthub_API/models.py
+++ backend/talenthub_backend/talenthub_API/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+
+class User(AbstractUser):
+    ROLE_CHOICES = [
+        ('not_assigned', 'Not Assigned'),
+        ('admin', 'Admin'),
+        ('employer', 'Employer'),
+        ('applicant', 'Applicant'),
+    ]
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='not_assigned')
+
+    def __str__(self):
+        return self.email
+
+
+class Job(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='jobs'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Application(models.Model):
+    STATUS_CHOICES = [
+        ('applied', 'Applied'),
+        ('shortlisted', 'Shortlisted'),
+        ('rejected', 'Rejected'),
+    ]
+    job = models.ForeignKey(Job, on_delete=models.CASCADE, related_name='applications')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='applications')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='applied')
+    applied_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} → {self.job.title} ({self.status})"
