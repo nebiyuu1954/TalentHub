62c1115f79cb97af1b61c56a6582e99a03f6680e
Nebiyu Main <nebiyuesaiyas1954@gmail.com>
2025-08-27 21:10:48 +0300

    migrated to sql lite

--- backend/talenthub_backend/talenthub_backend/settings.py
+++ backend/talenthub_backend/talenthub_backend/settings.py
@@ -86,17 +86,23 @@ WSGI_APPLICATION = 'talenthub_backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'talenthub_db',
+#         'USER': 'postgres',
+#         'PASSWORD': 'a',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'talenthub_db',
-        'USER': 'postgres',
-        'PASSWORD': 'a',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',  # Adjust the path as needed
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
